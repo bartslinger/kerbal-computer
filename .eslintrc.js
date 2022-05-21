47a25cd4dddff89d0bec41244239d2e5b8d75cf9
Bart Slinger <bartslinger@gmail.com>
2022-05-22 00:24:07 +0200

    install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    'standard'
-  ],
+  extends: ["plugin:vue/essential", "standard", "plugin:prettier/recommended"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
-}
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};
